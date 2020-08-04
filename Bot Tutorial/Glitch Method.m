09e5e717d53c244b262fea8dbc42ae09a70c4685
MoonLGH <66781351+MoonLGH@users.noreply.github.com>
2020-08-04 15:05:06 +0700

    Create Glitch Method.m

---
+++ Bot Tutorial/Glitch Method.m
@@ -0,0 +1,15 @@
+Okay hello again :D
+
+BTW during this pandamic dont forget to wash your hand first 
+
+after u done it lets get in to the tutorial
+
+## Tutorial
+
+### Step 1 get an [glitch](https://glitch.com/) account 
+### Step 2 click on new project and pick import form github <img src="https://cdn.discordapp.com/attachments/736854565449891891/740112711119274014/unknown.png">
+
+### Step 3 Put "https://github.com/MoonLGH/Mineflayer-DiscordJS-Template.git" to the url bar
+
+### Step 3,5(OPTIONAL) You can delete an useless stuff like Bot tutorial and readme.md
+### Step 4 Open 
